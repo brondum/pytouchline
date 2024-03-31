5b1d467106247e463672e277a046fecfbd2872c3
Peter Brøndum <peter.brondum@bestseller.com>
2024-03-31 15:23:20 +0200

    fixing release workflow and adding dependabot

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
   name = 'pytouchline_extended',
   packages = ['pytouchline_extended'],
-  version = '0.4',
+  version = '0.0.0',
   description = 'A Roth Touchline interface library',
+  long_description="A simple helper library for controlling a Roth Touchline heat pump controller",
   author = 'Peter Brondum',
   license='MIT',
   url = 'https://github.com/brondum/pytouchline',
-  download_url = 'https://github.com/brondum/pytouchline/archive/0.4.tar.gz',
   keywords = ['Roth', 'Touchline', 'Home Assistant', 'hassio', "Heat pump"],
   classifiers = [
 	'Development Status :: 3 - Alpha',
